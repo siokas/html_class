b251db4f797c87b497811baf3aed34de94dbf3f7
Apostolos Siokas <apostolossiokas@gmail.com>
2021-02-26 18:45:18 +0200

    quizzz

---
+++ 5. javascript/js/questions.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+        "answer": true,
+        "question": "Ο Spiderman δαγκώθηκε από αράχνη"
+    },
+    {
+        "answer": false,
+        "question": "Ο Clark Kent είναι ο Batman"
+    },
+    {
+        "answer": false,
+        "question": "Ο Captain America δεν εχει ασπίδα"
+    }
+];
