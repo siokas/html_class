2135b0d7c4e250043bff063d1b7205b38c62f53f
Apostolos Siokas <apostolossiokas@gmail.com>
2021-03-05 18:49:57 +0200

    wip

--- 5. javascript/js/questions.js
+++ 5. javascript/js/questions.js
@@ -5,10 +5,37 @@ var questions = [
     },
     {
         "answer": false,
-        "question": "Ο Clark Kent είναι ο Batman"
+        "question": "Ο Clark Kent είναι ο Batman",
+        "wiki": "Ο Batman ονομάζεται Bruce Wane. O Clark Kent είναι ο Superman!"
     },
-    {
-        "answer": false,
-        "question": "Ο Captain America δεν εχει ασπίδα"
-    }
+    // {
+    //     "answer": false,
+    //     "question": "Ο Captain America δεν εχει ασπίδα",
+    // },
+    // {
+    //     "answer": false,
+    //     "question": "Το σφυρί του Θορ ονομάζεται μελιόρντ",
+    //     "wiki": "Το σφυρί του Θορ ονομάζεται ΜΙΛΙΟΡΝΤ"
+    // },
+    // {
+    //     "answer": false,
+    //     "question": "Η ασπίδα του Κάπτεν Αμέρικα είναι κατασκευασμένη από Βοξύτη",
+    //     "wiki": "Είναι κατασκευασμένη από Βιμπράνιο"
+    // },
+    // {
+    //     "answer": true,
+    //     "question": "Στην ταινία Θορ-2 ο κόσμος κινδυνεύει από τα Σκοτεινά ξωτικά"
+    // },
+    // {
+    //     "answer": true,
+    //     "question": "Στην ταινία Θορ-2 η ασταθς ουσία που προσπαθούν να επανακτήσουν οι κακοί ώστε να επαναφέρουν το σκοτάδι στο σύμπαν ονομάζεται Αιθέρας"
+    // },
+    // {
+    //     "answer": true,
+    //     "question": "Η γυναίκα του Ιron Man ονομάζεται Virginia Pepper Pots"
+    // },
+    // {
+    //     "answer": true,
+    //     "question": "Ο ετεροθαλής αδελφός του Θορ ονομάζεται Λόκι"
+    // }
 ];
